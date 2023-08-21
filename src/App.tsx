0e1ba70bce9eac619e69ab157b7b8cb3c1bd0afb
sandro <sulpizi.a@northeastern.edu>
2023-08-21 19:08:56 -0400

    adding new functionality to read wallet tokens
    Co-authored-by: Harish Ravi <HR-31@users.noreply.github.com>
    Co-authored-by: Loïc Darboux <loicdrbx@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import {ethers} from "ethers";
 import "./App.css";
-import { LatestTransactions, SQLQuery } from "@sort/react-components";
+
 function App() {
+  const [balance, setBalance] = useState<number[]>([]);
+  const [error, setError] = useState<string | null>(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const provider = new ethers.providers.JsonRpcProvider(
+          "https://autumn-little-asphalt.avalanche-testnet.discover.quiknode.pro/35535d348097021d4a6a9be74c28d0f495c8140e/ext/bc/C/rpc/" //Avalanche testnet endpoint
+        );
+        provider.connection.headers = { "x-qn-api-version": 1 };
+        const heads = await provider.send("qn_getWalletTokenBalance", {
+          wallet: "0xABd59256fdB8d744D1A8daaE4F6c39C208C98336",
+          contracts: ["0xd00ae08403B9bbb9124bB305C09058E32C39A48c"] //this is wAVAX token address on testnet              
+        });
+        const fetchedBalance = heads.assets.map((bal: any) => bal.amount);
+        setBalance(fetchedBalance);
+      } catch (err: any) {
+        setError(`Error fetching balance: ${err.message}`);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <>
-      <h1>Hello World, we still don't know our group name!</h1>
-      {/* <LatestTransactions
-        contract_address="0x887f3909c14dabd9e9510128ca6cbb448e932d7f"
-        api_key="ec2fa3f2-7fdc-4342-9ebc-95f8b2bd7d21"
-        theme="dark"
-        blockchain="ethereum"
-      /> */}
-      <SQLQuery
-        query="select * from ethereum.transaction where from_address='0xd8da6bf26964af9d7eed9e03e53415d37aa96045' order by block_id desc"
-        api_key="ec2fa3f2-7fdc-4342-9ebc-95f8b2bd7d21"
-      />
-      <SQLQuery
-        query="select * from ethereum.transaction where to_address='0xABd59256fdB8d744D1A8daaE4F6c39C208C98336' order by block_id desc"
-        api_key="ec2fa3f2-7fdc-4342-9ebc-95f8b2bd7d21"
-      />
-    </>
+    <div className="App">
+      {error ? (
+        <p style={{ color: 'red' }}>{error}</p>
+      ) : (
+        balance.map((bal, index) => (
+          <p key={index}>Balance: {bal}</p>
+        ))
+      )}
+    </div>
   );
 }
 

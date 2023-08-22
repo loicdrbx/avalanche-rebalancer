96e5c5f9909e07760c379a016013190db6d3bc6f
sandro <sulpizi.a@northeastern.edu>
2023-08-22 13:06:47 -0400

    reverted html.index, converting app.tsx to infura
    Co-authored-by: Loïc Darboux <loicdrbx@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import {ethers} from "ethers";
+import { ethers } from "ethers";
 import "./App.css";
 
 function App() {
@@ -8,10 +8,9 @@ function App() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const provider = new ethers.providers.JsonRpcProvider(
-          "https://autumn-little-asphalt.avalanche-testnet.discover.quiknode.pro/35535d348097021d4a6a9be74c28d0f495c8140e/ext/bc/C/rpc/" //Avalanche testnet endpoint
+        const provider = new ethers.JsonRpcProvider(
+          "https://avalanche-fuji.infura.io/v3/39e0a61714e0484db229e80b2c686f27/" //Avalanche testnet endpoint from Infura
         );
-        provider.connection.headers = { "x-qn-api-version": 1 };
         const heads = await provider.send("qn_getWalletTokenBalance", {
           wallet: "0xABd59256fdB8d744D1A8daaE4F6c39C208C98336",
           contracts: ["0xd00ae08403B9bbb9124bB305C09058E32C39A48c"] //this is wAVAX token address on testnet              
